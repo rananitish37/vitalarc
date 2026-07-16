48c91687d48111cbd11333af501f0a5ea8c087fb
nrana <rananitish37@gmail.com>
2026-07-16 22:03:12 +0530

    fix MySQL user grants to cover both localhost and remote connections

--- mysql-init/01-create-databases.sql
+++ mysql-init/01-create-databases.sql
@@ -2,6 +2,11 @@ CREATE DATABASE IF NOT EXISTS vitalarc_users;
 CREATE DATABASE IF NOT EXISTS vitalarc_workouts;
 
 CREATE USER IF NOT EXISTS 'vitalarc'@'%' IDENTIFIED BY 'vitalarc';
+CREATE USER IF NOT EXISTS 'vitalarc'@'localhost' IDENTIFIED BY 'vitalarc';
+
 GRANT ALL PRIVILEGES ON vitalarc_users.* TO 'vitalarc'@'%';
+GRANT ALL PRIVILEGES ON vitalarc_users.* TO 'vitalarc'@'localhost';
 GRANT ALL PRIVILEGES ON vitalarc_workouts.* TO 'vitalarc'@'%';
+GRANT ALL PRIVILEGES ON vitalarc_workouts.* TO 'vitalarc'@'localhost';
+
 FLUSH PRIVILEGES;
